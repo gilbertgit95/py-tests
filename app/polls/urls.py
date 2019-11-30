aa51e08f226a6ece409416e6f4807d956858694f
Gilbert D. Cuerbo <gilbert.cuerbo@detailonline.tech>
2019-11-30 18:31:19 +0800

    add edit and add product form

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product', views.product, name='product'),
+    path('product_form', views.product_form, name='product_form'),
     path('polls', views.polls, name='polls'),
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,21 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+import math
+
 from . import models
 
 # Create your views here.
-from django.http import HttpResponse
-
 
 def home(request):
+
     products = models.Product.objects.all()
+    products = list(products)
+    ls_group_size = 4
 
-    return render(request, 'products/products_list.html', {'products': products})
+    # add item for add product
+    products.insert(0, {
+        'product_image': 'https://cdn2.iconfinder.com/data/icons/ecommerce-solid-version/32/add_product_new_product_item_ecommerceproduct--512.png',
+        'product_name': 'Add Product',
+        'highlighted': True
+    })
 
+    ls_group = math.ceil(len(products) / ls_group_size)
+    ls_count = 0
+    ls_result = []
+
+    while ls_count < ls_group:
+        start = ls_count * ls_group_size
+        end = start + ls_group_size
+
+        ls_result.append(products[start:end])
+        ls_count = ls_count + 1
+
+    return render(request, 'products/products_list.html', {'products': ls_result})
+
+@csrf_exempt
+def product_form(request):
+    product_id = request.GET.get('id', '')
+    template_data = {
+        'is_new': True,
+        'product': {}
+    }
+
+    try:
+        # product id means edit form
+        if len(product_id):
+            template_data['is_new'] = False
+            template_data['product'] = models.Product.objects.filter(id=product_id)[0]
+
+        # empty product id means new product
+        else:
+            template_data['is_new'] = True
+
+    # if error occurs during proccess redirect to home
+    except:
+        return redirect('/')
+
+    return render(request, 'products/product_form.html', template_data)
+
+@csrf_exempt
 def product(request):
     product_id = request.GET.get('id', '')
-    if len(product_id):
-        product = models.Product.objects.filter(id=product_id)[0]
+    try:
+        # use to display the product page
+        if (request.method == 'GET'):
+            product = models.Product.objects.filter(id=product_id)[0]
 
-    return render(request, 'products/product.html', {'product': product})
+            return render(request, 'products/product.html', {'product': product})
+
+        # use to add new product
+        elif (request.method == 'post'):
+            return JsonResponse({
+                'type': 'post',
+                'id': product_id
+            })
+
+        # use to edit a particular product
+        elif (request.method == 'PATCH'):
+            return JsonResponse({
+                'type': 'PATCH',
+                'id': product_id
+            })
+
+        # use to delete a product
+        elif (request.method == 'DELETE'):
+            return JsonResponse({
+                'type': 'DELETE',
+                'id': product_id
+            })
+
+        # redirect if method not 
+        else:
+            return redirect('/')
+
+    # redirect if an error occur
+    except:
+        return redirect('/')
+
 
 def polls(request):
     return HttpResponse("Hello, world. You're at the polls index.")
